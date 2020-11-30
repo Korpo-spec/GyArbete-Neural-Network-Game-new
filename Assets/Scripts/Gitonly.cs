e02fd82c41378ab7f5c82d29f2bd2b56b8abf1c3
Casper <Casper.sandstrom@elev.ga.ntig.se>
2020-11-30 16:34:40 +0100

    StartScréen and data transfer

---
+++ Assets/Scripts/Gitonly.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gitonly : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        actionsOut[0] = Input.GetAxisRaw("Horizontal") + 1;
+        actionsOut[1] = Input.GetAxisRaw("Vertical");
+        
+        if(Input.GetKey(KeyCode.Space)){
+            actionsOut[2] = 1;
+        }
+        else{
+            actionsOut[2] = 0;
+        }
+        */ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class StartScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    bool isGamer;
+    public void IsGamer(){
+        isGamer = true;
+    }
+
+    public void IsNotGamer(){
+        isGamer = false;
+        FileStream file = File.Open(@"Results.csv", FileMode.OpenOrCreate);
+        file.Close();
+        string[]  contents = File.ReadAllLines(@"Results.csv");
+    }
+}
